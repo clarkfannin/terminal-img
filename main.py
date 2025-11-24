478253c998eb6739296c48988f5b50dd4c0fd0c0
size12husky <jclarkfii@gmail.com>
2025-11-24 12:20:39 -0600

    Added support for transparent images and larger images

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 from PIL import Image
-from rich import print
 from rich.console import Console
+import shutil
 
-#PIL
-im = Image.open('image.jpg', 'r')
+terminal_width, terminal_height = shutil.get_terminal_size()
+
+
+console = Console() # rich
+
+
+im = Image.open('image.webp', 'r')
+orig_width, orig_height = im.size
+
+max_width = terminal_width - 4  # padding
+max_height = (terminal_height - 4) * 5 
+
+scale = min(max_width / orig_width, max_height / orig_height)
+new_width = int(orig_width * scale)
+new_height = int(orig_height * scale)
+
+im = im.resize((new_width, new_height))
 width, height = im.size
+
 pixel_values = list(im.getdata())
 
-#rich
-console = Console()
+
+
+left_padding = (terminal_width - width) // 2
 
 for i, pixel in enumerate(pixel_values):
-    r, g, b = pixel
+    r, g, b = pixel[:3]
     if i % width == 0:
-        print("\n")
-    console.print("█   ", style=f"rgb({r},{g},{b})", end="")
+        if i != 0:
+            console.print()
+        print(" " * left_padding, end="")
+    if r == 0 and g == 0 and b == 0:
+        console.print(" ", end="")
+    else:
+        console.print("█", style=f"rgb({r},{g},{b})", end="")
+
+console.print()
